a3a2be4bad4c32ec0f24bb56d39729f5993dc685
Savindu Gunasekara <g.savindu@gtngroup.com>
2025-04-27 15:58:03 +0530

    changes

--- app/api/adminAuth/route.ts
+++ app/api/adminAuth/route.ts
@@ -3,22 +3,27 @@ import prisma from '@/app/lib/db';
 import bcrypt from 'bcrypt';
 
 export async function POST(req: Request) {
-  const { password } = await req.json(); // Extract password from request body
+  try {
+    const { password } = await req.json(); // Extract password from request body
 
-  const user = await prisma.user.findUnique({
-    select: { password: true },
-    where: { id: 1 },
-  });
+    const user = await prisma.user.findUnique({
+      select: { password: true },
+      where: { id: 1 },
+    });
 
-  if (!user || !user.password) {
-    return NextResponse.json({ success: false, message: 'User not found' }, { status: 404 });
+    if (!user || !user.password) {
+      return NextResponse.json({ success: false, message: 'User not found' }, { status: 404 });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      return NextResponse.json({ success: true });
+    }
+
+    return NextResponse.json({ success: false }, { status: 401 });
+  } catch (error) {
+    console.error('Error in /api/adminAuth route:', error);
+    return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
   }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-
-  if (isMatch) {
-    return NextResponse.json({ success: true });
-  }
-
-  return NextResponse.json({ success: false }, { status: 401 });
 }
