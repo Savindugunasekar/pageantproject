7dda1673840973fd81f40f3b3219c88a3b7d322d
Savindu Gunasekara <g.savindu@gtngroup.com>
2025-05-03 08:50:04 +0530

    genie fixed

--- components/PaymentHandler.tsx
+++ components/PaymentHandler.tsx
@@ -16,7 +16,7 @@ const PaymentHandler = () => {
       setIsLoading(true);
 
      
-     await axios.post("/api/geniebiz-payment", {
+     const response = await axios.post("/api/geniebiz-payment", {
         "amount": 600.00,
         "currency": "LKR",
         "localId": "Test dialog txn local id",
@@ -27,7 +27,13 @@ const PaymentHandler = () => {
       }
       });
 
-   
+      const paymentUrl = response.data?.url;
+
+    if (paymentUrl) {
+      window.location.href = paymentUrl; // Redirect to GenieBiz payment page
+    } else {
+      console.error("No payment URL returned from API");
+    }
 
 
 
