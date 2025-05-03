7a21fb41ebedd1290a348d9e4cbb7c7bd76e9592
Savindu Gunasekara <g.savindu@gtngroup.com>
2025-05-03 08:32:01 +0530

    url part

--- app/api/geniebiz-payment/route.ts
+++ app/api/geniebiz-payment/route.ts
@@ -5,6 +5,9 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
+    console.log("Body:", body);
+    
+
     const response = await axios.post(
       "https://api.uat.geniebiz.lk/public/v2/transactions",
       body,
