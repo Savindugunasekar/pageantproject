5b9970b6868c78ec77c2cde20c4ad36d1b47055a
Savindu Gunasekara <g.savindu@gtngroup.com>
2025-05-13 08:14:36 +0530

    UI fixes done

--- app/api/fetchContestants/route.ts
+++ app/api/fetchContestants/route.ts
@@ -1,11 +1,16 @@
 // app/api/test/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/app/lib/db';
+
 export async function GET(req: NextRequest) {
   try {
-    // Fetch all rows from the 'test' table
-    const contestantList = await prisma.contestants.findMany();
-    
+    // Fetch all rows from the 'contestants' table ordered by contestantId ascending
+    const contestantList = await prisma.contestants.findMany({
+      orderBy: {
+        id: 'asc',
+      },
+    });
+
     // Return the fetched data as JSON
     return NextResponse.json(contestantList);
   } catch (error) {
